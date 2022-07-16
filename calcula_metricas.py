da668adb89f220833e6a54e32d63a4f2ce480f8e
victorncg <victorncg@gmail.com>
2022-07-16 19:01:58 -0300

    Update calcula_metricas.py

--- calcula_metricas.py
+++ calcula_metricas.py
@@ -3,10 +3,35 @@ import pandas as pd
 
 
 def razao_preco_media(stock, start, mm):
+    
+    #import yfinance as yf
+    #import pandas as pd
+
     data = yf.download(stock, start = start)
 
     data['media'] = data.Close.rolling(mm).mean()
     
+
+    #weg['ret_dia'] = weg.Close.pct_change().rolling(30).mean()
+
+    #weg['vol'] = weg.Close.pct_change().rolling(30).std()
+
+    #weg['razao_ret_vol'] = weg['ret_dia']/weg['vol']
+
+    data['razao'] = data['Close']/data['media']
+
+    return data['razao']
+
+
+def plota_razao_preco_media(stock, start, mm):
+
+    #import yfinance as yf
+    #import pandas as pd
+
+    data = yf.download(stock, start = start)
+
+    data['media'] = data.Close.rolling(mm).mean()
+
 
     #weg['ret_dia'] = weg.Close.pct_change().rolling(30).mean()
 
