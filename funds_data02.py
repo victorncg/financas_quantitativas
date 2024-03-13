3c41329b5bc57f05d0734471d11863d4ce6fb177
Victor Gomes <victorncg@gmail.com>
2024-03-13 20:30:58 -0300

    Update funds_data02.py

--- funds_data02.py
+++ funds_data02.py
@@ -31,6 +31,7 @@ def _logging_error(func):
 
 @_logging_error
 def create_interval(start_date: object, end_date: object):
+    
     """
     This function is responsable for receiving user input for start and end dates for data extraction and transforming it into YYYYMM format.
     
@@ -38,22 +39,27 @@ def create_interval(start_date: object, end_date: object):
     :end_date: str
     """
     
-    b = list()
-    end = str(datetime.datetime.strptime(end_date, '%Y-%m-%d').year) + '{:02d}'.format(datetime.datetime.strptime(end_date, '%Y-%m-%d').month +1)
-    
-    for year in range(int(datetime.datetime.strptime(start_date, '%Y-%m-%d').year), int(datetime.datetime.strptime(end_date, '%Y-%m-%d').year)+1):
+  b = list()
+
+  end = str(datetime.datetime.strptime(end_date, '%Y-%m-%d').year) + '{:02d}'.format(datetime.datetime.strptime(end_date, '%Y-%m-%d').month +1)
+
+  for year in range(int(datetime.datetime.strptime(start_date, '%Y-%m-%d').year), int(datetime.datetime.strptime(end_date, '%Y-%m-%d').year)+1):
+
     for month in range(1,13):
+
       a = '{:02d}{:02d}'.format(year, month)
+
       if a == end:
         break
-    
+
       b.append(a)
-    
+
     if a == end:
         break
+
     year = year +1
-    
-    return b
+
+  return b
 
 
 @_logging_error
@@ -65,30 +71,35 @@ def extract_fund_data(dates_list: list, verbose: bool = False):
     :verbose: bool
     """
     
-    start_time = time.time()
-    # Initialize an empty list to store individual DataFrames
-    dataframes = []
-    
-    # Iterate through the files in the folder
-    for i in dates_list:
+  start_time = time.time()
+
+  # Initialize an empty list to store individual DataFrames
+  dataframes = []
+
+  # Iterate through the files in the folder
+  for i in dates_list:
+
     url_pre = f'https://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_{i}.zip'
+
     df_loc = pd.read_csv(url_pre, sep=';', compression='zip')
+
     s = datetime.datetime.strptime(i, "%Y%m")
-    date = s.strftime('%B %Y')
     
+    date = s.strftime('%B %Y')
+
     if verbose == True:
       print("Extraction of month",date,"finished")
     
     # Append the DataFrame to the list
     dataframes.append(df_loc)
-    
-    # Combine all DataFrames in the list into one large DataFrame
-    combined_df = pd.concat(dataframes, ignore_index=True)
-    
-    if verbose == True:  
+
+  # Combine all DataFrames in the list into one large DataFrame
+  combined_df = pd.concat(dataframes, ignore_index=True)
+
+  if verbose == True:  
     print("Process took %s seconds" % (time.time() - start_time))
-    
-    return combined_df
+
+  return combined_df
 
 
 
@@ -103,16 +114,22 @@ def get_funds_data(start: object, end: object, cnpj:object = None, verbose: bool
     :verbose: bool
     """
     
-    interval = create_interval(start, end)
-    data_fund = extract_fund_data(interval, verbose)
-    data_fund['DT_COMPTC'] = pd.to_datetime(data_fund['DT_COMPTC'])
-    mask = (data_fund['DT_COMPTC'] >= start) & (data_fund['DT_COMPTC'] <= end)
-    data_fund = data_fund.loc[mask]
+  interval = create_interval(start, end)
+
+  data_fund = extract_fund_data(interval, verbose)
+
+  data_fund['DT_COMPTC'] = pd.to_datetime(data_fund['DT_COMPTC'])
+
+  mask = (data_fund['DT_COMPTC'] >= start) & (data_fund['DT_COMPTC'] <= end)
+
+  data_fund = data_fund.loc[mask]
+
+  if cnpj != None:
     
-    if cnpj != None:
     data_fund = data_fund[data_fund['CNPJ_FUNDO'] == cnpj]
-    
-    else:
+
+  else:
+
     data_fund = data_fund
-    
-    return data_fund.reset_index(drop=True)
+  
+  return data_fund.reset_index(drop=True)
